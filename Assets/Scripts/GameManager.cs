31b1ed1fe6101465791fb07e1083acb259ab117f
⛧ <matthias.goffette@telecom-sudparis.eu>
2019-09-19 11:30:29 +0200

    GameManager: Basic GameManager
    Spawns player on startup
    Spawns enemies every so often
    Dimensions are badly handled with magic values, this will be fixed

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField]
+    private PlayerAvatar playerPrefab;
+
+    [SerializeField]
+    private EnemyAvatar enemyPrefab;
+
+    [SerializeField]
+    private float enemySpawnInterval;
+    public float EnemySpawnInterval {
+        get {
+            return this.enemySpawnInterval;
+        }
+        private set {
+            this.enemySpawnInterval = value;
+        }
+    }
+
+    private float lastEnemySpawn;
+
+    private PlayerAvatar player;
+    public PlayerAvatar Player {
+        get {
+            return this.player;
+        }
+        private set {
+            this.player = value;
+        }
+    }
+
+    float worldHeight;
+    float heightOrigin;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Instantiate Player
+        PlayerAvatar player = Instantiate(playerPrefab);
+        lastEnemySpawn = 0;
+        worldHeight = 10;
+        heightOrigin = 5;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // every so often, instantiate enemy at random height
+        lastEnemySpawn += Time.deltaTime;
+        if (lastEnemySpawn > EnemySpawnInterval) {
+            float height = Random.Range(heightOrigin - worldHeight, heightOrigin);
+            Instantiate(enemyPrefab, new Vector2(6.6f, height), Quaternion.identity);
+            lastEnemySpawn = 0;
+        }
+    }
+}
